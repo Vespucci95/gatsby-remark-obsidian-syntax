ac7a168b9410a2ee58f9bfd0a7b09bc23177d783
Vespucci95 <donglonk@naver.com>
2025-05-21 16:17:53 +0900

    style: 코드 정리

--- src/convertToObsidianSyntax.ts
+++ src/convertToObsidianSyntax.ts
@@ -8,39 +8,51 @@ export const convertTag: ConvertObsidianSyntax = (text, {
   toHashTagUrl,
   className = DEFAULT_OBSIDIAN_CLASSNAME
 }) => {
-  return text.replace(OBSIDIAN_MARKDOWN_REGEX.tag, wrapWithTag('a', '$1', {
-    class: className.tag,
-    href: toHashTagUrl('$1')
-  }));
+  return text.replace(
+    OBSIDIAN_MARKDOWN_REGEX.tag,
+    wrapWithTag('a', '$1', {
+      class: className.tag,
+      href: toHashTagUrl('$1')
+    })
+  );
 };
 
 export const convertHighlightText: ConvertObsidianSyntax = (text, {
   className = DEFAULT_OBSIDIAN_CLASSNAME
 }) => {
-  return text.replace(OBSIDIAN_MARKDOWN_REGEX.highlight, wrapWithTag('span', '$1', {
-    class: className?.highlight
-  }));
+  return text.replace(
+    OBSIDIAN_MARKDOWN_REGEX.highlight,
+    wrapWithTag('span', '$1', {
+      class: className.highlight
+    }));
+
 };
 
 export const convertInternalLink: ConvertObsidianSyntax = (text, {
   toPageUrl,
   className = DEFAULT_OBSIDIAN_CLASSNAME
 }) => {
-  return text.replace(OBSIDIAN_MARKDOWN_REGEX.internalLink, wrapWithTag('a', '$1', {
-    class: className?.internalLink,
-    href: toPageUrl('$1')
-  }));
+  return text.replace(
+    OBSIDIAN_MARKDOWN_REGEX.internalLink,
+    wrapWithTag('a', '$1', {
+      class: className.internalLink,
+      href: toPageUrl('$1')
+    })
+  );
 };
 
 export const convertEmbedImage: ConvertObsidianSyntax = (text, {
   toImageUrl,
   className = DEFAULT_OBSIDIAN_CLASSNAME
 }) => {
-  return text.replace(OBSIDIAN_MARKDOWN_REGEX.embedImage, wrapWithTag('img', null, {
-    class: className?.embedImage,
-    src: toImageUrl('$1'),
-    placeholder: '$1'
-  }));
+  return text.replace(
+    OBSIDIAN_MARKDOWN_REGEX.embedImage,
+    wrapWithTag('img', null, {
+      class: className.embedImage,
+      src: toImageUrl('$1'),
+      placeholder: '$1'
+    })
+  );
 };
 
 const hasObsidianSyntax = (text: string): boolean => {
@@ -67,6 +79,7 @@ const convertToObsidianSyntax = (phrasingContent: PhrasingContent, options: Obsi
   }
 
   return {
+    ...phrasingContent,
     type: 'html',
     value: extendObsidianSyntax(value, options)
   };
